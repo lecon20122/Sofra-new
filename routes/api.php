34e7448f634d2ae68f61646ad434ad33ac1e681f
lecon20122 <65439060+lecon20122@users.noreply.github.com>
2021-05-23 23:49:46 +0200

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\URL;
+
+if (App::environment('production')) {
+    URL::forceScheme('https');
+}
+
 //USER-AUTH
 Route::post('register', 'App\Http\Controllers\Auth\AuthController@register');
 Route::post('login', 'App\Http\Controllers\Auth\AuthController@login');
