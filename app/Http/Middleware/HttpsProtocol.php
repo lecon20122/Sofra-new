79ea7c28438ad6844447853455a09652557115d1
lecon20122 <65439060+lecon20122@users.noreply.github.com>
2021-05-24 16:53:29 +0200

    Update HttpsProtocol.php

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -16,10 +16,10 @@ class HttpsProtocol
      */
     public function handle(Request $request, Closure $next)
     {
-        if (!$request->secure()) {
+        if (!$request->secure() && app()->environment('production')) {
             return redirect()->secure($request->getRequestUri());
         }
 
-        return $next($request); 
+        return $next($request);
     }
 }
