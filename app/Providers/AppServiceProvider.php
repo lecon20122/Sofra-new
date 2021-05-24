b3b9b221409bac5dc8c176b38d89c611dae76e3b
lecon20122 <65439060+lecon20122@users.noreply.github.com>
2021-05-24 16:07:56 +0200

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
-use Illuminate\Support\Facades\URL;
+// use Illuminate\Support\Facades\URL;
 
 
 
@@ -29,6 +29,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-        URL::forceScheme('https');
+//         URL::forceScheme('https');
     }
 }
