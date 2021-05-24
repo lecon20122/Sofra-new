5b728c29921fc62f5e6246eaf6ce3c5cb70dbf52
lecon20122 <lecon20122@gmail.com>
2021-05-24 13:19:48 +0200

    fixing conflict

--- routes/api.php
+++ routes/api.php
@@ -2,12 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\App;
-use Illuminate\Support\Facades\URL;
+// use Illuminate\Support\Facades\App;
+// use Illuminate\Support\Facades\URL;
 
-if (App::environment('production')) {
-    URL::forceScheme('https');
-}
+// if (App::environment('production')) {
+//     URL::forceScheme('https');
+// }
 //USER-AUTH
 Route::post('register', 'App\Http\Controllers\Auth\AuthController@register');
 Route::post('login', 'App\Http\Controllers\Auth\AuthController@login');
