af55eb6ffbfeb480060ba45d8b99fce02b21f1e9
lecon20122 <65439060+lecon20122@users.noreply.github.com>
2021-05-24 16:57:30 +0200

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\App;
-use Illuminate\Support\Facades\URL;
+// use Illuminate\Support\Facades\App;
+// use Illuminate\Support\Facades\URL;
 
-if (App::environment('production')) {
-    URL::forceScheme('https');
-}
+// if (App::environment('production')) {
+//     URL::forceScheme('https');
+// }
 
 
 //USER-AUTH
